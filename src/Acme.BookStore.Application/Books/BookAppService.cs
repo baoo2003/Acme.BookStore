0a67bc8b806d52f9740bacce16106d452d882a57
Bảo Vũ <vuquocbao673@gmail.com>
2025-06-22 16:52:11 +0700

    feat: API Book

---
+++ src/Acme.BookStore.Application/Books/BookAppService.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Volo.Abp.Application.Dtos;
+using Volo.Abp.Application.Services;
+using Volo.Abp.Domain.Repositories;
+
+namespace Acme.BookStore.Books
+{
+    public class BookAppService :
+    CrudAppService<
+        Book,
+        BookDto,
+        Guid, //PK of book entity
+        PagedAndSortedResultRequestDto,
+        CreateUpdateBookDto>,
+    IBookAppService
+    {
+        public BookAppService(IRepository<Book, Guid> repository)
+            : base(repository)
+        {
+
+        }
+    }
+}
